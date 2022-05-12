8ce07dd145ed657667d508548e8b9c7bfe3e4a39
Cezar <cezarlantosgmail.com>
2022-05-12 16:49:42 +0200

    Aggiungere i file di progetto.

---
+++ ShowMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_es03_mattina
+{
+    internal class ShowMessage
+    {
+        public static void AppendMessage(string text)
+        {
+            /*
+            if (text.Length > 10)
+            {
+                Console.WriteLine(text.Substring(0, 10));
+                AppendMessage(text.Substring(10));
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+            */
+
+
+            
+            while (text.Length > 10)
+            {
+                Console.WriteLine(text.Substring(0, 10));
+                text = text.Substring(10);
+            }
+            Console.WriteLine(text);
+            
+
+            /*
+            int pos = 0;
+            foreach (char c in text)
+            {
+                if (pos == 10)
+                {
+                    Console.WriteLine(c);   
+                    pos = 0;
+                }
+                Console.WriteLine(c);
+                pos++;
+            }
+            */
+
+        }
+    }
+}
